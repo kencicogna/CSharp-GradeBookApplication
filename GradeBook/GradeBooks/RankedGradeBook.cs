853ff15a081829ceb89aa271455242bc2fede665
Ken Cicogna <Ken.Cicogna@acxiom.com>
2019-03-13 16:36:23 -0500

    calc stats - fixed message

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -37,7 +37,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 or more students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 or more students.");
                 return;
             }
 
@@ -48,7 +48,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
             {
-                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                Console.WriteLine("Ranked grading requires at least 5 students.");
                 return;
             }
 
